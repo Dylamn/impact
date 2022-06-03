e1026d01c845ecfd0705633da311df03a831dcc7
Dylan <dylan.vallee@hotmail.com>
2022-06-03 11:33:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Impact
+
+The goal of this project is to improve, at maximum as possible, the overall performance and evaluate the ecodesign 
+of any website.
+
+
+### Building psycopg2 sources
+[Link to the documentation](https://www.psycopg.org/docs/install.html#install-from-source)
---
+++ authentication/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as __
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    email = models.EmailField(__('email address'), unique=True)
