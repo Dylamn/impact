1bab017436491c845d334209a536234bbfac9c53
Dylan <dylan.vallee@hotmail.com>
2022-06-23 15:19:12 +0200

    Added signup view + django form

---
+++ accounts/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import User
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            "username", "email", "first_name", "last_name",
+            "password1", "password2",
+        ]
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect, reverse
+
+from .forms import SignupForm
 
 
-# Create your views here.
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    status = 200
+
+    if request.user.is_authenticated:
+        return redirect(reverse('landing'))
+
+    if request.method == "POST":
+        # `is_active` default at True because
+        # there's no email verification currently.
+        form = SignupForm(request.POST, initial={"is_active": True})
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)  # Log in the new user.
+
+            return redirect(reverse('landing'))
+        else:
+            # Validation error append
+            status = 400
+    else:
+        form = SignupForm()
+
+    ctx = {"form": form}
+
+    return render(request, 'accounts/signup.html', context=ctx, status=status)
