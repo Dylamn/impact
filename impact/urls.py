a2cc700413d096b42d4670de524f7af43e98a0b6
Dylan <dylan.vallee@hotmail.com>
2023-01-19 17:52:40 +0100

    Prefix pathname with the locale + dropdown for
    language selection

--- impact/urls.py
+++ impact/urls.py
@@ -1,15 +1,19 @@
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 
 from impact import views
 
 urlpatterns = [
-    path('', views.LandingView.as_view(), name='landing'),
-    path('terms/', views.TermsView.as_view(), name='terms'),
-
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('metrics/', include('metrics.urls')),
 
     path('api/', include('metrics_api.urls'))
 ]
+
+urlpatterns += i18n_patterns(
+    path('', views.LandingView.as_view(), name='landing'),
+    path('terms/', views.TermsView.as_view(), name='terms'),
+    path('', include('accounts.urls')),
+    path('metrics/', include('metrics.urls')),
+)
